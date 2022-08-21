de57d00c8085dedaf5e39fa27c5c5ce51b16cf3f
Kaz <82215788+Kazdev0@users.noreply.github.com>
2022-08-21 15:26:46 +0300

    Added Discord Login

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+require("../authentication/passport/discord");
 const {getUserLogin, postUserLogin, getUserRegister, postUserRegister, getUserLogout} = require("../controllers/userControllers")
 
 router.get("/login",getUserLogin);
@@ -12,5 +14,13 @@ router.post("/login",postUserLogin);
 
 router.post("/register",postUserRegister)
 
+router.get("/discord/login", passport.authenticate("discord"));
+router.get('/discord/callback', passport.authenticate('discord', {
+    failureRedirect: '/login'
+}), function(req, res) {
+    res.redirect('/') // Successful auth
+});
+
+  
 
 module.exports = router;
